3242695190cb56cf05373e8d2448c23789bd57cc
Andriy <andrijbuluk@gmail.com>
2026-04-23 15:46:27 +0200

    Taxi service authentication

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,6 +7,7 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    ViewInfoDriver,
 )
 
 urlpatterns = [
@@ -19,9 +20,8 @@ urlpatterns = [
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("info-user/<int:pk>/", ViewInfoDriver.as_view(), name="info-user" ),
 ]
 
 app_name = "taxi"
